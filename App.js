ef71485fcc388b3dd124836b7cee61cb028481a9
kondo27 <95572834+kondo27@users.noreply.github.com>
2022-01-16 12:05:55 +0900

    Rifactorring ShoppingList

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Appbar from './src/components/Appbar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/elements/CircleButton';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Appbar />
+        <MemoList />
+        <CircleButton>+</CircleButton>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
